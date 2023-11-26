57d048162c1ecd3ab51a7268757275aab4637223
kenroly <ngthedat2002@gmail.com>
2023-11-26 09:44:07 +0700

    pagination for main page

---
+++ Final-project/assets/js/pagination.js
@@ -0,0 +1,77 @@
+let currentPage = 1;
+const pageSize = 15;
+
+async function fetchData(page) {
+    const response = await fetch(`https://ecomws.didongviet.vn/fe/v1/products?page=${page}&limit=${pageSize}&category_ids=3&filterParams=sort_by_outstanding&sort_by_outstanding=true`);
+    const data = await response.json();
+    return data;
+}
+
+function displayData(data) {
+    const products = document.getElementById('products');
+    products.innerHTML = '';
+    data.data.data.forEach(i => {
+        let card = document.createElement("div")
+        card.classList.add("card")
+
+        let imgContainer = document.createElement("div")
+        imgContainer.classList.add("image-container")
+
+        let image = document.createElement("img")
+        image.src = "https://cdn-v2.didongviet.vn/" + i.thumbnail
+        imgContainer.appendChild(image)
+        card.appendChild(imgContainer)
+
+        let container = document.createElement("div")
+        container.classList.add("container")
+
+        let productName = document.createElement("h5")
+        productName.classList.add("product-name")
+        productName.innerHTML = i.product
+        container.appendChild(productName)
+
+        let price = document.createElement("h6")
+        price.innerHTML = i.price + "đ"
+        container.appendChild(price)
+        price.style.color = "red"
+        price.style.fontSize = "20px"
+
+        let nomalprice = document.createElement("h6")
+        nomalprice.innerHTML = i.list_price + "đ"
+        container.appendChild(nomalprice)
+        nomalprice.style.textDecoration = "line-through"
+
+        let addButton = document.createElement("button")
+        addButton.innerHTML = "THÊM VÀO GIỎ HÀNG"
+        addButton.classList.add("addButton")
+        container.appendChild(addButton)
+
+        card.appendChild(container)
+
+        document.getElementById("products").appendChild(card)
+    });
+}
+
+function setupPagination() {
+    document.getElementById('prev-page').addEventListener('click', () => {
+        if (currentPage > 1) {
+            currentPage -= 1;
+            fetchDataAndDisplay();
+        }
+    });
+
+    document.getElementById('next-page').addEventListener('click', () => {
+        currentPage += 1;
+        fetchDataAndDisplay();
+    });
+}
+
+function fetchDataAndDisplay() {
+    fetchData(currentPage).then(data => {
+        displayData(data);
+        document.getElementById('current-page').innerText = currentPage;
+    });
+}
+
+fetchDataAndDisplay();
+setupPagination();
